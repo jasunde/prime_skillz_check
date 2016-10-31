8db4d93ab4f6ae19cba3c947c0b81afa8b74dddf
jasunde <jlaurits@gmail.com>
2016-10-31 15:06:38 -0500

    Completed folder 3

--- 3-conditionals/if.js
+++ 3-conditionals/if.js
@@ -4,9 +4,18 @@
 var first = 3;
 var second = 5;
 
+if(first == second) {
+  console.log('Hello');
+}
+
 // 2 - Expand your answer above to
 // console log "Goodbye" if
 // the variables are not equal
+if(first == second) {
+  console.log('Hello');
+} else {
+  console.log('Goodbye');
+}
 
 // 3 - Write a statement that
 // assigns the value of the addition
@@ -15,3 +24,10 @@ var second = 5;
 // than second. If second is greater than first
 // assign the difference of the first & second
 // variables to third.
+if(first > second) {
+  var third = first + second;
+}
+
+if(second > first) {
+  var third = first - second;
+}
--- 3-conditionals/switch.js
+++ 3-conditionals/switch.js
@@ -1,4 +1,23 @@
-// Write a switch statement that 
+// Write a switch statement that
 // console logs witty statements for each
 // day of the standard work week: Monday, Tuesday,
 // Wednesday, Thursday, Friday
+var day = 'Tuesday';
+
+switch(day) {
+  case 'Monday':
+    console.log('Not again...');
+    break;
+  case 'Tuesday':
+    console.log('Meh.');
+    break;
+  case 'Wednesday':
+    console.log('Yay! Hump day!');
+    break;
+  case 'Thursday':
+    console.log('Almost there...!');
+    break;
+  case 'Friday':
+    console.log('TGI Friday!! Let\'s hava party!!');
+    break;
+}
