0bf22777b1c36dfd24a47d90b59ec275f6038fa7
jasunde <jlaurits@gmail.com>
2016-10-31 16:04:24 -0500

    Completed folder 9

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -1,7 +1,12 @@
 	// 1 - After the DOM loads, add a paragraph tag that says "Page loaded"
 	// to the section element
+$(document).ready(function () {
+  $('section').append('<p>Page loaded</p>');
 
 	// 2 - Change the text of the h1 element to something (anything!) else.
+  $('h1').text('Something else!');
 
   // 3 - Get rid of (delete) the useless unordered list (ul) by first
   // selecting the h2 nested within its list item.
+  $('h2').closest('ul').remove();
+});
